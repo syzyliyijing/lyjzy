3456595f59347bcf4df61e75ecd51540ef0a4003
syzyliyijing <3123235594@qq.com>
2019-10-21 11:20:19 +0800

    作业10.21~

--- node01.js
+++ node01.js
@@ -2,6 +2,6 @@ var http=require('http');
 http.createServer(function(request,response){
     //发送HTTP头部
     response.writeHead(200,{'Content-Type':'text/plain'});
-    response.end('Hello World\n');
+    response.end('Hello World 192.\n');
 }).listen(7777);
 console.log('Server running at http://127.0.0.1:7777/');
